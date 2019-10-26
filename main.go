19a6c04da6ccc84d28b455dae3a7ea851a91d557
user0111 <vladbarista@mail.ru>
2019-10-26 14:30:48 +0300

    Setup orm db config

--- main.go
+++ main.go
@@ -2,9 +2,17 @@ package main
 
 import (
 	_ "github.com/Ksos-org/ksos-api/routers"
-
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/orm"
+	_ "github.com/astaxie/beego/orm"
+	_ "github.com/lib/pq"
 )
+
+func init() {
+	_ = orm.RegisterDriver("postgres", orm.DRPostgres)
+
+	_ = orm.RegisterDataBase("default", "postgres", "dbname=postgres host=localhost user=postgres password=postgres port=5432 sslmode=disable")
+}
 
 func main() {
 	if beego.BConfig.RunMode == "dev" {
